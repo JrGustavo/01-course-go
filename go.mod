2ccd6c7f2f52ed676651563da20f418c9ead683e
JrGustavo <gus@soypetal.com>
2024-07-19 15:12:03 -0500

    Update Variables

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JrGustavo/01-course-go
+
+go 1.22rc1
---
+++ variables/enteras.go
@@ -0,0 +1,9 @@
+package variables
+
+func MeustroEnteros() {
+	var intcomun int
+	intde32 := int32(10)
+	intede64 := int64(100)
+	intede64 := int64(100)
+
+}
